801ecfd0165bf23f587c288b3ed57e0406b03e09
Wit <witalian.zajac@gmail.com>
2018-09-24 10:36:24 +0200

    initial

---
+++ FirstNativeAndroidApp/FirstNativeAndroidApp/src/com/FirstNativeAndroidApp/FirstNativeAndroidApp.java
@@ -0,0 +1,17 @@
+
+package com.FirstNativeAndroidApp;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class FirstNativeAndroidApp extends SDLActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
